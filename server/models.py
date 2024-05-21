595dfb351201277a022bd9a37a1a6538b93fb8d8
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-05-21 16:41:56 +0800

    // server/model.py: remove unused code

--- server/models.py
+++ server/models.py
@@ -48,8 +48,6 @@ def get_model(model_cfg: DictConfig):
         model = AutoPeftModelForCausalLM.from_pretrained(
             model_cfg.peft_path, torch_dtype=torch.float16
         ).to("cuda")
-        base_model = model.peft_config["default"].base_model_name_or_path
-        tokenizer = AutoTokenizer.from_pretrained(base_model)
 
     model = prepare_model_for_kbit_training(
         model, use_gradient_checkpointing=model_cfg.gradient_checkpointing
