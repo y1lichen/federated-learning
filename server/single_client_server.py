6d8311ab047fa550d50d74a08cdd6943dc4514d2
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-06-04 01:44:36 +0800

    save peft model for inference

--- server/single_client_server.py
+++ server/single_client_server.py
@@ -4,14 +4,19 @@ import argparse
 import os
 from hydra import compose, initialize
 from save_model_strategy import SaveModelStrategy
-from utils.utils import get_on_fit_config, fit_weighted_average, get_init_parameters
+from utils.utils import (
+    get_on_fit_config,
+    fit_weighted_average,
+    get_init_parameters,
+    get_evaluate_fn,
+)
 
 with initialize(config_path="conf"):
     cfg = compose(config_name="config")
 NUM_ROUNDS = cfg.num_rounds
 
 
-save_path = "./results/"
+save_path = "./results/peft/"
 if not os.path.exists(save_path):
     os.mkdir(save_path)
 
@@ -23,6 +28,9 @@ def main(num_clients=1, num_rounds=NUM_ROUNDS) -> None:
         min_available_clients=num_clients,
         on_fit_config_fn=get_on_fit_config(),
         initial_parameters=get_init_parameters(),
+        evaluate_fn=get_evaluate_fn(
+            cfg, cfg.train.save_every_round, cfg.num_rounds, save_path
+        ),
     )
 
     # Start Flower server
--- server/utils/utils.py
+++ server/utils/utils.py
@@ -8,6 +8,7 @@ import glob
 import os
 import numpy as np
 from typing import List
+from models import get_model
 
 
 # Get a function that will be used to construct the config that the client's
@@ -105,3 +106,20 @@ def get_init_parameters() -> Parameters:
     initial_parameters = [v.numpy() for v in state_dict.values()]
     initial_parameters = flwr.common.ndarrays_to_parameters(initial_parameters)
     return initial_parameters
+
+
+def get_evaluate_fn(model_cfg, save_every_round, total_round, save_path):
+
+    def evaluate(server_round: int, parameters, config):
+        # Save model
+        if server_round != 0 and (
+            server_round == total_round or server_round % save_every_round == 0
+        ):
+            # Init model
+            model = get_model(model_cfg)
+            set_parameters(model, parameters)
+            model.save_pretrained(f"{save_path}/peft_{server_round}")
+
+        return 0.0, {}
+
+    return evaluate
