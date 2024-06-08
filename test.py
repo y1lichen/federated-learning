8ec2692a4edd81cda81bb32f0b164070aeedf714
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-06-08 14:48:41 +0800

    update test file

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-# This python file is adapted from https://github.com/lm-sys/FastChat/blob/main/fastchat/llm_judge/gen_model_answer.py
-
-import torch
 from transformers import AutoTokenizer, TextStreamer
 from hydra import compose, initialize
 from server.utils.utils import set_parameters, get_init_parameters
-from transformers import AutoModelForCausalLM
 from server.models import get_model
 import flwr
 
@@ -12,8 +8,15 @@ import flwr
 with initialize(config_path="server/conf"):
     cfg = compose(config_name="config")
 
-INSTRUCTION = "你是我的朋友，請你以朋友的口氣回答以下："
-INPUT = "你要去上統計學嗎"
+
+INSTRUCTION = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+# INSTRUCTION = "你是我的朋友，請你以朋友的口氣回答以下："
+# INPUT = "明天要一起吃早餐嗎"
+# INPUT = "健嗎"
+# INPUT = "你微甲作業寫了？"
+INPUT = "你要去上統計學？"
+# INPUT = "我睡過頭了"
+
 MODEL_NAME = cfg.model.name
 
 # Load model and tokenizer
@@ -25,16 +28,21 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 streamer = TextStreamer(tokenizer=tokenizer)
 # Generate answers
-inputs = tokenizer(f"{INSTRUCTION}{INPUT}", return_tensors="pt")
-output = model.generate(
+system_prompt = f"[INST] {INSTRUCTION} [/INST]\n [USER] {INPUT} [/USER]"
+inputs = tokenizer(system_prompt, return_tensors="pt")
+outputs = model.generate(
     **inputs,
     streamer=streamer,
-    temperature=0.7,
+    temperature=0.62,
     max_new_tokens=512,
-    repetition_penalty=0.8,
-    max_time=30.0,
+    repetition_penalty=1.15,
+    max_time=60.0,
 )
-output_text = tokenizer.decode(output[0], skip_special_tokens=False)
 
 print(">>> Ouput")
-print(output_text)
+for i, sample_output in enumerate(outputs):
+    print(
+        "文案{}: {}".format(
+            i + 1, tokenizer.decode(sample_output, skip_special_tokens=True)
+        )
+    )
